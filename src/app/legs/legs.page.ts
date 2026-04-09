0acadf8ae1327fb2b5d8d23ae49a3730b90c4530
unknown <G00471112@atu.ie>
2026-04-09 20:45:30 +0000

    Add Ionic back buttons and navigation headers across muscle pages

--- src/app/legs/legs.page.ts
+++ src/app/legs/legs.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-legs',
   templateUrl: './legs.page.html',
   styleUrls: ['./legs.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class LegsPage implements OnInit {
 
