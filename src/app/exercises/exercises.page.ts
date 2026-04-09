a5d04e4c45d181365fa63ced52e725725706c02a
unknown <G00471112@atu.ie>
2026-04-09 21:37:12 +0000

    Load exercises from JSON using WorkoutService with HttpClient and display dynamically in Exercises page

--- src/app/exercises/exercises.page.ts
+++ src/app/exercises/exercises.page.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonCard, IonCardHeader, IonCardTitle, IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { WorkoutService } from '../services/workout.service';
 
 @Component({
   selector: 'app-exercises',
   templateUrl: './exercises.page.html',
   styleUrls: ['./exercises.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCard, IonCardHeader, IonCardTitle, IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ExercisesPage implements OnInit {
 
-  constructor() { }
+  exercises: any[] = [];
+
+  constructor(private workoutService: WorkoutService) { }
 
   ngOnInit() {
+    this.workoutService.getExercises().subscribe(data => {
+      this.exercises = data;
+    });
   }
 
 }
